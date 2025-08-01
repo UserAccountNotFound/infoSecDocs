d9beed69554f0afb892643a8de6b4b0a0c16d89e
msp <msp@netgroup.su>
2025-08-01 17:02:21 +0300

    Добавлены шрифты (полностью отключены шрифты google), обработчик висящих строк. Исправлена верстка таблиц

---
+++ mkdocs_text_handlers/plugin.py
@@ -0,0 +1,20 @@
+from mkdocs.plugins import BasePlugin
+from mkdocs.config.config_options import Type
+import re
+
+class NonBreakingSpacePlugin(BasePlugin):
+    config_scheme = (
+        ('enabled', Type(bool, default=True)),
+        ('prepositions', Type(list, default=['АО', 'ООО','ПАО', 'а', 'в', 'на', 'и', 'о', 'к', 'с', 'со','за', '(за', 'об', 'не', '№', '"О', '«О', 'ее', 'её'])),
+    )
+    
+    def on_page_content(self, html, page, config, files):
+        if not self.config['enabled']:
+            return html
+        
+        pattern = re.compile(
+            r'(?<!\w)(' + '|'.join(re.escape(p) for p in self.config['prepositions']) + r')(\s+)',
+            re.IGNORECASE
+        )
+        
+        return pattern.sub(lambda m: f"{m.group(1)}&nbsp;", html)
