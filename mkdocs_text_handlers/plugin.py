7e45c3f03da27d061a84a28112a6a47bf71c34d7
jumly <jumly@yandex.ru>
2025-08-01 18:29:21 +0000

    обновлен обработчик висящих строк

--- mkdocs_text_handlers/plugin.py
+++ mkdocs_text_handlers/plugin.py
@@ -5,16 +5,44 @@ import re
 class NonBreakingSpacePlugin(BasePlugin):
     config_scheme = (
         ('enabled', Type(bool, default=True)),
-        ('prepositions', Type(list, default=['АО', 'ООО','ПАО', 'а', 'в', 'на', 'и', 'о', 'к', 'с', 'со','за', '(за', 'об', 'не', '№', '"О', '«О', 'ее', 'её'])),
+        ('prepositions', Type(list, default=['АО', 'ООО','ПАО', 'а', 'в', '(в','на', 'и', 'о', 'к', 'с', 'со','за', '(за', 'об', 'не', '№', '"О', '«О', 'ее', 'её'])),
     )
     
     def on_page_content(self, html, page, config, files):
         if not self.config['enabled']:
             return html
         
+        # Обработка предлогов
         pattern = re.compile(
             r'(?<!\w)(' + '|'.join(re.escape(p) for p in self.config['prepositions']) + r')(\s+)',
             re.IGNORECASE
         )
+        html = pattern.sub(lambda m: f"{m.group(1)}&nbsp;", html)
         
-        return pattern.sub(lambda m: f"{m.group(1)}&nbsp;", html)
+        # Обработка цифр с единицами измерения
+        units_pattern = re.compile(
+            r'(\d+)(\s+)(г|кг|мм|см|м|км)\b',
+            re.IGNORECASE
+        )
+        html = units_pattern.sub(lambda m: f"{m.group(1)}&nbsp;{m.group(3)}", html)
+
+        # Обработка инициалов и слов вокруг них
+        initials_pattern = re.compile(
+            r'(\s|^)([А-ЯЁа-яёA-Za-z])\.\s*([А-ЯЁа-яёA-Za-z])\.|'       # А. Б.
+            r'(\w+)\s+([А-ЯЁа-яёA-Za-z])\.\s*([А-ЯЁа-яёA-Za-z])\.|'     # слово А.Б.
+            r'([А-ЯЁа-яёA-Za-z])\.\s*([А-ЯЁа-яёA-Za-z])\.\s+(\w+)'      # А.Б. слово
+        )
+        
+        def replace_initials(match):
+            if match.group(2) and match.group(3):                                           # Случай "А. Б."
+                return f"{match.group(1)}{match.group(2)}.&nbsp;{match.group(3)}."
+            elif match.group(5) and match.group(6):                                         # Случай "слово А.Б."
+                return f"{match.group(4)}&nbsp;{match.group(5)}.&nbsp;{match.group(6)}."
+            elif match.group(7) and match.group(8):                                         # Случай "А.Б. слово"
+                return f"{match.group(7)}.&nbsp;{match.group(8)}.&nbsp;{match.group(9)}"
+            return match.group(0)
+        
+        html = initials_pattern.sub(replace_initials, html)
+        
+        
+        return html
